ac23d93a585d878daaaf972ad25ab0fe6b44a4ad
Weelapat <38096723+BillyBacker@users.noreply.github.com>
2022-04-20 17:57:39 +0700

    Added to Vector

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import uvicorn
 import json
 
 cnfg = {
-    "HOST":"0.0.0.0",
+    "HOST":"localhost",
     "PORT":"9001"
 }
 
@@ -35,5 +35,21 @@ async def getBoard(size:str, ratio:float):
         }
     return JSONResponse(status_code=200, content=pk)
 
+@app.get("/vector/{size}/{ratio}")
+async def getBoard(size:str, ratio:float):
+    row, column = map(int, size.split("-"))
+    g = graph((row,column))
+    random(g, n=(row*column*ratio))
+    pk = {
+            "numberOfRows": g.size[0],
+            "numberOfCols": g.size[1],
+            "clues": genClues(g)
+        }
+    String = "Vector(\n"
+    for i, clue in enumerate(pk['clues']):
+        String += f" ({clue['position']['x']}, {clue['position']['y']}, {clue['size']}){',' if i < len(pk['clues'])-1 else ''}\n"
+    String += ")"
+    print(String)
+    return String
 if __name__ == "__main__":
     uvicorn.run("main:app", host=f'{cnfg["HOST"]}', port=int(cnfg["PORT"]), reload=False)
